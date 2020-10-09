4d623c8326461d162a65c64418972fc17b05c191
Trevor Warthman <43298111+Trevor-Warthman@users.noreply.github.com>
2020-10-09 17:08:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "intToRoman.js"
 import "romanToInt.js"
       
-romans = ["I","IV","V","VI","VII","VIII","IX","X","XL","L","XC","C","CD","D","CM","M"]
-numbers = [1,4,5,6,7,8,9,10,40,50,90,100,400,500,900,1000]
+var romans = ["I","IV","V","VI","VII","VIII","IX","X","XL","L","XC","C","CD","D","CM","M"]
+var numbers = [1,4,5,6,7,8,9,10,40,50,90,100,400,500,900,1000]
     
 function detectTypeAndConvert(romans,numbers) {
     var inp = document.getElementById("inputText").value;
