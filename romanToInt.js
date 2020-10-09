9b154724d35b5908fd83525893819a3bec1e23a8
Trevor Warthman <43298111+Trevor-Warthman@users.noreply.github.com>
2020-10-09 12:49:52 -0400

    Create romanToInt.js

---
+++ romanToInt.js
@@ -0,0 +1,18 @@
+//Note: Easier than making roman numerals. Just take each part one at a time.
+//Given a roman numeral, convert it to an integer.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    if (s.length == 0)
+        return 0
+    var current = 0
+    let i = 15
+    while (romans[i] != s.substring(0,romans[i].length)){
+        i--
+    }
+    s = s.substring(romans[i].length,s.length)
+    return (numbers[i] + romanToInt(s))
+};
