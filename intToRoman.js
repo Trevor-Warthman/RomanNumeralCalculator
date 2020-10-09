b8a80534062766f2cef692f0fc3c4c0f4b45de9a
Trevor Warthman <43298111+Trevor-Warthman@users.noreply.github.com>
2020-10-09 12:48:17 -0400

    Create intToRoman.js

---
+++ intToRoman.js
@@ -0,0 +1,21 @@
+//Recursive Function which returns an integer's string roman numeral value
+// @param {Integer} num
+// @return {String}
+// @Requires 1 < num < 4000
+//@Params: 
+// - num: the integer
+// - array of roman numerals 1-10,40,50,90,100,400,500,900,1000
+// - array of numbers same as above
+
+var intToRoman = function(num,romans,numbers)
+    if (num == 0){
+        return ""
+    }
+    var i = 15
+    while (num - numbers[i] < 0){
+        i--
+    }
+    num = num - numbers[i]
+    current = romans[i]
+    return current + int_to_roman(num,romans,numbers)
+end
