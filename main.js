7d8ebb4987c880ae81edeb8461202202c017ead2
Trevor Warthman <43298111+Trevor-Warthman@users.noreply.github.com>
2020-10-09 14:04:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 import intToRoman from intToRoman.js
-    import romanToInt from romanToInt.js
+import romanToInt from romanToInt.js
       
-    romans = ["I","IV","V","VI","VII","VIII","IX","X","XL","L","XC","C","CD","D","CM","M"]
-    numbers = [1,4,5,6,7,8,9,10,40,50,90,100,400,500,900,1000]
+romans = ["I","IV","V","VI","VII","VIII","IX","X","XL","L","XC","C","CD","D","CM","M"]
+numbers = [1,4,5,6,7,8,9,10,40,50,90,100,400,500,900,1000]
     
-    function detectTypeAndConvert(romans,numbers) {
-      var inp = document.getElementById("inputText").value;
-      var out = ""
-      if (Number.isInteger(inp)){
+function detectTypeAndConvert(romans,numbers) {
+    var inp = document.getElementById("inputText").value;
+    var out = ""
+    if (Number.isInteger(inp)){
         out = intToRoman(parseInt(inp),romans,numbers)
-      }
-      else{
-        out = romanToInt(toUpperCase(inp),romans,numbers)
-      }
-      //else{
-      //  out = "Please enter a number or roman numeral between 1 and 4000."
-      //}
-      document.getElementById("demo").innerHTML = out;
     }
+    else{
+      out = romanToInt(toUpperCase(inp),romans,numbers)
+    }
+    //else{
+    //  out = "Please enter a number or roman numeral between 1 and 4000."
+    //}
+    document.getElementById("demo").innerHTML = out;
+}
